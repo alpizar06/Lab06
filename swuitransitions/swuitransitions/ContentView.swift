e9292aab77724e30b92b740ed459643c4609447a
Daniel Alpizar <alpizar0610@gmail.com>
2021-09-04 12:56:14 -0600

    Add files via upload

---
+++ swuitransitions/swuitransitions/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  swuitransitions
+//
+//  Created by Daniel Alpizar on 4/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var isShowingSquare = false
+    
+    var body: some View {
+        VStack{
+        Button ("Tap Me"){
+            withAnimation{
+                self.isShowingSquare.toggle()
+            }
+            
+        }
+            if isShowingSquare{
+                Rectangle()
+                .fill(Color.purple)
+                .frame(
+                width: 200,
+                height: 200,
+                alignment: .center)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
+        
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swuitransitions/swuitransitions/swuitransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swuitransitionsApp.swift
+//  swuitransitions
+//
+//  Created by Daniel Alpizar on 4/9/21.
+//
+
+import SwiftUI
+
+@main
+struct swuitransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
